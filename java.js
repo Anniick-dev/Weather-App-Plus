84e20bcfc975277007448ed3963f0f0ef4f4181c
Anniick-dev <141575965+Anniick-dev@users.noreply.github.com>
2023-08-25 11:18:43 +0200

    Adjusted temperature & description text, along with search bar hover

--- java.js
+++ java.js
@@ -128,15 +128,10 @@ function getCurrentLocation(event) {
   function showWeather(response) {
     document.querySelector("#city").innerHTML = response.data.city;
     let celsiusTemperature = Math.round(response.data.temperature.current);
-    let minTemp = Math.round(response.data.temperature.minimum);
-    let maxTemp = Math.round(response.data.temperature.maximum);
+    let feelTemp = Math.round(response.data.temperature.feels_like);
     let descriptionText = response.data.condition.description;
     document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
-    document.querySelector("#description").innerHTML = `It is a day filled with ${descriptionText} & a minimum temperature of ${minTemp} and a maximum of ${maxTemp}. <small>
-    Humidity: <span class="hum" id="hum"></span>%
-<br/>
-Wind: <span class="wind" id="wind"></span>
-<span class="windUnit">km/h</span> Enjoy!`;
+    document.querySelector("#description").innerHTML = `It is a day filled with ${descriptionText} & feels like ${feelTemp}&degC. Enjoy!`;
     document.querySelector("#hum").innerHTML = response.data.temperature.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     
@@ -172,47 +167,51 @@ function updateTemperatureUnits() {
   let temperatureUnitsElement = document.querySelector(".temperatureUnits");
   let windElement = document.querySelector("#wind");
   let windUnitElement = document.querySelector(".windUnit");
-  
+
   let minTempElement = document.querySelector("#minTemp");
   let maxTempElement = document.querySelector("#maxTemp");
 
   if (unitToggle.checked) {
-    // Celsius to Fahrenheit conversion
+    // Celsius to Fahrenheit conversion for current temperature
     let celsiusTemperature = parseFloat(temperatureElement.textContent);
     let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
     temperatureElement.textContent = Math.round(fahrenheitTemperature);
     temperatureUnitsElement.innerHTML = "&deg;F";
 
+    // Celsius to Fahrenheit conversion for wind speed
     let windCelsius = parseFloat(windElement.textContent);
-    let windFahrenheit = (windCelsius * 0.621371);
+    let windFahrenheit = windCelsius * 0.621371;
     windElement.textContent = Math.round(windFahrenheit);
     windUnitElement.innerHTML = "m/h";
 
+    // Celsius to Fahrenheit conversion for min and max temperatures
     let minCelsiusTemperature = parseFloat(minTempElement.textContent);
     let maxCelsiusTemperature = parseFloat(maxTempElement.textContent);
     let minFahrenheitTemperature = (minCelsiusTemperature * 9) / 5 + 32;
     let maxFahrenheitTemperature = (maxCelsiusTemperature * 9) / 5 + 32;
     minTempElement.textContent = Math.round(minFahrenheitTemperature);
     maxTempElement.textContent = Math.round(maxFahrenheitTemperature);
-    } else {
-    // Fahrenheit to Celsius conversion
+  } else {
+    // Fahrenheit to Celsius conversion for current temperature
     let fahrenheitTemperature = parseFloat(temperatureElement.textContent);
     let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
     temperatureElement.textContent = Math.round(celsiusTemperature);
     temperatureUnitsElement.innerHTML = "&deg;C";
 
+    // Fahrenheit to Celsius conversion for wind speed
     let windFahrenheit = parseFloat(windElement.textContent);
     let windCelsius = windFahrenheit / 0.621371;
     windElement.textContent = Math.round(windCelsius);
     windUnitElement.innerHTML = "km/h";
 
+    // Fahrenheit to Celsius conversion for min and max temperatures
     let minFahrenheitTemperature = parseFloat(minTempElement.textContent);
     let maxFahrenheitTemperature = parseFloat(maxTempElement.textContent);
     let minCelsiusTemperature = ((minFahrenheitTemperature - 32) * 5) / 9;
     let maxCelsiusTemperature = ((maxFahrenheitTemperature - 32) * 5) / 9;
     minTempElement.textContent = Math.round(minCelsiusTemperature);
     maxTempElement.textContent = Math.round(maxCelsiusTemperature);
-}
+  }
 }
 
 var unitToggle = document.querySelector(".switch-control-input");
